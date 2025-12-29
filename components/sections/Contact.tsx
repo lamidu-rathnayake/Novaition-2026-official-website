5df892dba13e1dc47e6fbf7b177647848a888b6d
lamidu <lamidurathnayakeofficial.com>
2025-12-29 22:58:03 +0530

    aggested some alignments

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -2,17 +2,17 @@
 
 export default function Contact() {
     return (
-        <section className="page-container py-4 w-full bg-muted text-foreground flex flex-col items-center justify-center h-auto md:h-screen">
+        <section className="page-container py-4 w-full bg-muted text-foreground flex flex-col items-center justify-start h-auto md:h-screen">
 
             {/* Title */}
-            <h2 className="text-5xl md:text-6xl font-bold text-center py-12 tracking-wider text-white uppercase">
+            <h2 className="flex-1 text-5xl md:text-6xl font-bold text-center py-12 tracking-wider text-white uppercase">
                 CONTACT US
             </h2>
 
-            <div className="w-full mx-auto flex flex-col md:flex-row items-stretch justify-center gap-8 md:gap-0 relative">
+            <div className="w-full flex-8 mx-auto flex flex-col md:flex-row items-stretch justify-center gap-8 md:gap-0 relative">
 
                 {/* Left Column: Hotlines */}
-                <div className="w-full md:w-1/2 flex flex-col items-center md:items-start gap-2 md:pl-0 text-center md:text-left">
+                <div className="w-full md:w-1/2 flex flex-col items-center justify-center md:items-start gap-2 md:pl-0 text-center md:text-left">
                     <h3 className="text-3xl md:text-5xl font-bold uppercase text-white mb-2">
                         OUR HOTLINES
                     </h3>
@@ -26,7 +26,7 @@ export default function Contact() {
                 <div className="hidden md:block w-1 bg-white absolute left-1/2 top-0 bottom-0 -translate-x-1/2"></div>
 
                 {/* Right Column: Email & Location */}
-                <div className="w-full md:w-1/2 flex flex-col items-center md:items-end gap-12 md:pr-0 text-center md:text-right">
+                <div className="w-full md:w-1/2 flex flex-col justify-center items-center md:items-end gap-12 md:pr-0 text-center md:text-right">
 
                     {/* Email */}
                     <div className="flex flex-col items-center md:items-end gap-2">
