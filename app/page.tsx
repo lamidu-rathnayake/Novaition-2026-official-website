62335e9de302490cc6d3fb6a5a695fee4bd9ce5b
lamidu <lamidurathnayakeofficial.com>
2025-12-30 13:56:39 +0530

    i added clothing section and some changes to university section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from '@/components/sections/HeroSection';
 import AboutSection from '@/components/sections/AboutSection';
 import Speakers from '@/components/sections/Speakers';
+import Clothing from '@/components/sections/Clothing';
 import Contact from '@/components/sections/Contact';
 import University from '@/components/sections/University';
 import Sponsers from '@/components/sections/Sponsers';
@@ -11,6 +12,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <Speakers />
+      <Clothing />
       <University />
       <Sponsers />
       <Contact />
