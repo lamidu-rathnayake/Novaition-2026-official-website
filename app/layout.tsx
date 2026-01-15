8cc1e0cb7f7282bda494897ffb998f160d9b894b
lamidu <lamidurathnayakeofficial.com>
2026-01-15 18:03:22 +0530

    remove unnecessary font imports and references in global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,14 @@
 import type { Metadata } from "next";
-import { Orbitron, Rajdhani,  } from "next/font/google";
 import "./globals.css";
 import Preloader from "@/components/ui/Preloader";
 import { Bebas_Neue } from 'next/font/google';
 
 const bebasNeue = Bebas_Neue({
-  weight: '400',       // Bebas Neue only supports 400 on Google Fonts
+  weight: '400',
   subsets: ['latin'],
   variable: "--font-bebas",
   display: 'swap',
 })
-
-const orbitron = Orbitron({
-  subsets: ["latin"],
-  variable: "--font-orbitron",
-  display: "swap",
-});
-
-const rajdhani = Rajdhani({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-rajdhani",
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "Novaition 2026",
@@ -37,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${orbitron.variable} ${rajdhani.variable} ${bebasNeue.variable} antialiased`}
+        className={`${bebasNeue.variable} antialiased`}
         suppressHydrationWarning
       >
         <Preloader />
